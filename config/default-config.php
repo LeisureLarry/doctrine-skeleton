30d3379eaf0d547dc33765001ef1b8905764ed2a
jteriete <j.teriete@interiete.net>
2014-07-25 12:12:32 +0200

    Added default configuration file

---
+++ config/default-config.php
@@ -0,0 +1,16 @@
+<?php
+
+// MySQL database configuration
+$connectionOptions = array(
+    'driver' => 'pdo_mysql',
+    'host' => 'localhost',
+    'user' => 'root',
+    'password' => '',
+    'dbname' => '',
+);
+
+// Application/Doctrine configuration
+$applicationOptions = array(
+    'debug_mode' => true, // in production environment false
+    'entity_dir' => dirname(__DIR__) . '/src/Models',
+);
